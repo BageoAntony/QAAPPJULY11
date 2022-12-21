57223cb1adb8178d6962c5071d70e4734750a4ed
anj-krish13 <arunimanikuleasn7076@gmail.com>
2022-12-21 15:24:36 +0530

    wednesdayproject

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from api.models import Questions
+
+
+class QuestionForm(forms.ModelForm):
+
+    class Meta:
+        model=Questions
+        fields=["title","description","image"]
+        widgets={
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+            "description":forms.Textarea(attrs={"class":"form-control","rows":5}),
+            "image":forms.FileInput(attrs={"class":"form-select"})
+        }
 
 class UserRegistrationForm(UserCreationForm):
 
