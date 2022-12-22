ea1d9a1d6904fd7e76b9a60a9b465d1cc33fcbaa
nihal8454 <muhammednihal8454@gmail.com>
2022-12-22 15:04:50 +0530

    thursdayproject

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignInView,SignUpView,IndexView
+from .views import SignInView,SignUpView,IndexView,add_answer,answer_upvote_view
 urlpatterns = [
     path("",SignInView.as_view(),name="signin"),
     path("register",SignUpView.as_view(),name="signup"),
-    path("index",IndexView.as_view(),name="index")
+    path("index",IndexView.as_view(),name="index"),
+    path("questions/<int:id>/answers/add",add_answer,name="add-answer"),
+    path("answers/<int:id>/upvotes/add",answer_upvote_view,name="add-upvote")
+
 ]
--- web/views.py
+++ web/views.py
@@ -5,15 +5,27 @@ from django.views.generic import CreateView,FormView,TemplateView,ListView
 from .forms import LoginForm,UserRegistrationForm,QuestionForm
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
-from api.models import Questions
+from django.contrib.auth import authenticate,login,logout
+from api.models import Questions,Answers
+from django.utils.decorators import method_decorator
+
+def signin_required(fn):
+    def wrapper(request,*args,**kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request,"invalid sesseion")
+            return redirect("signin")
+        else:
+            return fn(request,*args,**kwargs)
+    return wrapper
 
 
+@method_decorator(signin_required,name="dispatch")
 class SignUpView(CreateView):
     template_name="register.html"
     form_class=UserRegistrationForm
     success_url=reverse_lazy("signin")
 
+@method_decorator(signin_required,name="dispatch")
 class SignInView(FormView):
     template_name="login.html"
     form_class=LoginForm
@@ -29,6 +41,7 @@ class SignInView(FormView):
             else:
                 return render(request,self.template_name,{"form":form})
 
+@method_decorator(signin_required,name="dispatch")
 class IndexView(CreateView,ListView):
     template_name="index.html"
     form_class=QuestionForm
@@ -42,6 +55,25 @@ class IndexView(CreateView,ListView):
         return super().form_valid(form)
     def get_queryset(self):
         return Questions.objects.exclude(user=self.request.user).order_by('-created_date')
+
+@signin_required
+def add_answer(request,*args,**kwargs):
+    id=kwargs.get("id")
+    ques=Questions.objects.get(id=id)
+    ans=request.POST.get("answer")
+    Answers.objects.create(question=ques,answer=ans,user=request.user)
+    messages.success(request,"your answer posted successfully")
+    return redirect("index")
+
+@signin_required
+def answer_upvote_view(request,*args,**kwargs):
+    id=kwargs.get("id")
+    ans=Answers.objects.get(id=id)
+    ans.upvote.add(request.user)
+    return redirect("index")
+
+
+
 
 
 
